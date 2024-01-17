c6f25b699c0e654c2d90e0c49ed3de1c16c430ee
lihaySwiss <lihayswissa@gmail.com>
2024-01-17 14:38:39 +0200

    start making state machine

--- source/Lexer.cpp
+++ source/Lexer.cpp
@@ -1,5 +1,28 @@
-#include "header/Lexer.h"
+#include <iostream>
+#include <fstream>
+#include "Token.cpp"
+
 #define COMMENT_SIGN '#'
+#define QUOTES '\''
+#define DOUBLE_QUOTES '\"'
+
+using namespace std;
+
+class Lexer
+{
+
+private:
+    std::string _lastCollectedValue;
+    ifstream *file;
+    
+public:
+    
+    Lexer(ifstream *file);
+
+    void readFromFile();
+    Token getToken(std::string currWord);
+};
+
 
 Lexer::Lexer(ifstream *file) {
     this->file = file;
@@ -13,12 +36,12 @@ void Lexer::readFromFile()
 
     while((*file) >> currWord)
     {
-         // skip whitespace
+        // skip whitespace
         while(!isspace(currWord[i]) && currWord[i] != ';') 
         {
             //getline((*file), skip, COMMENT_SIGN); //skip comments
 
-            std::cout <<  getToken(currWord.substr(i)).type << std::endl;
+            std::cout <<  getToken(currWord).type << std::endl;
             i++;
         }
         i = 0;
@@ -30,7 +53,8 @@ void Lexer::readFromFile()
 Token Lexer::getToken(std::string currWord)
 {
     std::cout << currWord << std::endl;
-
+    Token* t = new Token();
     
-    return t;
+    t->type = "ID";
+    return (*t);
 }
